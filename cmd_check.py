64510308da0d2d837a34f385ad61f9f295a2f909
parth025sharma <parthsharalj12@ff.com>
2019-06-11 15:55:06 +0530

    Command check and save

--- cmd_check.py
+++ cmd_check.py
@@ -1,9 +1,28 @@
 #!/usr/bin/python3
-import os
-cmd=input("Enter word to find if it is a command or not:-")
-#returned_value = os.system("echo $?")
-if os.system("echo $?")==0:
-        print(cmd + "is a command")
-        os.system(cmd)
-else :
-        print("It is not a comand")
+import os,sys
+import subprocess as sp
+f=open("command.txt","a")
+y='Y'
+while y=='Y':
+        cmd=input("Enter word to find if it is a command or not:-")
+        p=sp.getoutput( cmd+ " >/dev/null ; echo $?")
+        c=cmd
+        if p=='0':
+                    print(cmd + " is a command")
+                    os.system(cmd)
+                    f.write(cmd+" \n")
+
+
+        else:
+                     print(cmd +" It is not a command") 
+                     f.write(cmd+"\n")
+        y=input("If you want to enter more press Y(capital):")
+        cmd=input("Enter word to find if it is a command or not:-")
+        if c==cmd:
+                os.system("echo not to do this again | festival --tts")
+                sys.exit()
+        else:
+                continue
+
+
+f.close()
